d0e686c65375b28e61bed03d237b7b9bd8c0a37e
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-09-22 15:11:03 +0200

    Modif width

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-    
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/theogerritsen/cktvgvy4d294h18lp92dm804n/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2dlcnJpdHNlbiIsImEiOiJja3R2Zzkybzkwa25oMm5tcGp1MWY0enh1In0.n_ye_r9ELbLqxyWl-giSlA', {
+const width = document.getElementById("map").offsetWidth*0.7,
+    margin = {top: 70, right: 20, bottom: 0, left: 120},
+    height = 600;
+
+const mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/theogerritsen/cktvgvy4d294h18lp92dm804n/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2dlcnJpdHNlbiIsImEiOiJja3R2Zzkybzkwa25oMm5tcGp1MWY0enh1In0.n_ye_r9ELbLqxyWl-giSlA', {
        attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
        tileSize: 512,
        zoomOffset: -1
 });
 
-var map = L.map('map')
+const map = L.map('map')
   .addLayer(mapboxTiles)
-  .setView([46.3610, 6.94], 11);
+  .setView([46.315119251778235, 6.93942686688034], 13);
