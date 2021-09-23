ed6ba7a5686fd76d90ac8f821af041d29bc04d73
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-09-23 12:18:00 +0200

    Insertion de trois couches de base (interchangeables)

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ const width = document.getElementById("map").offsetWidth*0.7,
     margin = {top: 70, right: 20, bottom: 0, left: 120},
     height = 600;
 
+const myMap = L.map('map').setView([46.315119251778235, 6.93942686688034], 13);
+  
+myMap.setMaxBounds([[46, 6], [47,7.5]]);
+myMap.setMinZoom(11);
+myMap.setMaxZoom(18);
+
+// création des const avec nos trois couches de bases (mapbox, google sat et google terrain)
 const mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/theogerritsen/cktvgvy4d294h18lp92dm804n/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2dlcnJpdHNlbiIsImEiOiJja3R2Zzkybzkwa25oMm5tcGp1MWY0enh1In0.n_ye_r9ELbLqxyWl-giSlA', {
        attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
        tileSize: 512,
        zoomOffset: -1
 });
 
-const map = L.map('map', {
-  maxBounds: [[46, 6], [47,7.5]],
-    minZoom: 11,
-    maxZoom: 18,
-    zoom: 13
-}
-)
-  .addLayer(mapboxTiles)
-  .setView([46.315119251778235, 6.93942686688034]);
+const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+  tileSize: 256,
+  subdomains:['mt0','mt1','mt2','mt3']
+});
+
+const googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+tileSize: 256,
+  subdomains:['mt0','mt1','mt2','mt3']
+});
+
+// on ajout la couche qui s'affichera lors du load de la page
+mapboxTiles.addTo(myMap);
+
+// on insère nos trois couches dans un variable
+const baseMaps = {
+  "Mapbox": mapboxTiles,
+  "Satellite": googleSat,
+  "Terrain": googleTerrain
+};
+
+const overlays = {};
+
+// on ajoute le controleur en haut a droite pour changer de couche à notre guise
+L.control.layers(baseMaps, overlays).addTo(myMap);
+
+
