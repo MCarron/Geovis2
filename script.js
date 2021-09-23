6182ac028b7e4a60be6ae4ead2a8c6f76c52b31f
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-09-23 14:55:44 +0200

    Ajout dûne échelle et modif du controleur de layer

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const myMap = L.map('map').setView([46.315119251778235, 6.93942686688034], 13);
 myMap.setMaxBounds([[46, 6], [47,7.5]]);
 myMap.setMinZoom(11);
 myMap.setMaxZoom(18);
+//////////////////////////////
+////  COUCHES DE BASE ////////
+/////////////////////////////
 
 // création des const avec nos trois couches de bases (mapbox, google sat et google terrain)
 const mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/theogerritsen/cktvgvy4d294h18lp92dm804n/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2dlcnJpdHNlbiIsImEiOiJja3R2Zzkybzkwa25oMm5tcGp1MWY0enh1In0.n_ye_r9ELbLqxyWl-giSlA', {
@@ -39,5 +42,11 @@ const overlays = {};
 
 // on ajoute le controleur en haut a droite pour changer de couche à notre guise
 L.control.layers(baseMaps, overlays).addTo(myMap);
+
+// ajout de l'échelle à la carte
+
+L.control.scale({
+  position: 'bottomleft'
+}).addTo(myMap);
 
 
