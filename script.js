348960cf5b96f37d6176f7ebe89d7b283f2155ca
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-11-09 10:02:21 +0100

    Nettoyage du js + meilleures commentaires

--- script.js
+++ script.js
@@ -6,61 +6,67 @@ function closeNav() {
   document.getElementById("myFilter").style.width = "0";
 }
 
-// localisation lors du load de la page
+/**
+* Implémentation de la carte 
+*/
 var myMap = L.map('map', {center: [46.33, 6.97],
-  minZoom: 10,
-  maxZoom: 18,
-  zoom: 13,
-  contextmenu: true,
-  contextmenuWidth: 140,
-  contextmenuItems: [{
-    text: 'Define as start point',
-    callback: choseStartPoint
-  }]});
+	minZoom: 10,
+	maxZoom: 18,
+	zoom: 13,
+	contextmenu: true,
+	contextmenuWidth: 140,
+	contextmenuItems: [{
+    	text: 'Define as start point',
+    	callback: choseStartPoint
+	}]
+});
 
 //////////////////////////////
 ////  COUCHES DE BASE ////////
 /////////////////////////////
 
-// création des const avec nos trois couches de bases (mapbox, google sat et google terrain)
+// Ajout de nos couches de base (layers)
 const mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/theogerritsen/cktvgvy4d294h18lp92dm804n/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2dlcnJpdHNlbiIsImEiOiJja3R2Zzkybzkwa25oMm5tcGp1MWY0enh1In0.n_ye_r9ELbLqxyWl-giSlA', {
-       attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a>',
-       tileSize: 512,
-       zoomOffset: -1
+    attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a>',
+    tileSize: 512,
+       oomOffset: -1
 });
 
 const osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+  	attribution: '&copy; OpenStreetMap contributors'
 });
 
 const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-  tileSize: 256,
-  subdomains:['mt0','mt1','mt2','mt3']
+  	tileSize: 256,
+  	subdomains:['mt0','mt1','mt2','mt3']
 });
 
 const googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
-tileSize: 256,
-  subdomains:['mt0','mt1','mt2','mt3']
+	tileSize: 256,
+  	subdomains:['mt0','mt1','mt2','mt3']
 });
 
-// on ajoute la couche qui s'affichera lors du load de la page
+//Ajout de la couche pour le load de la page
+
 osmLayer.addTo(myMap);
 
-
-// on insère nos trois couches dans un variable
+/**
+ * Ajout des 4 couches dans une variables pour
+ * le baseMaps controler
+ */
 const baseMaps = {
-  "OpenStreetMap": osmLayer,
-  "Mapbox": mapboxTiles,
-  "Satellite": googleSat,
-  "Terrain": googleTerrain
+  	"OpenStreetMap": osmLayer,
+  	"Mapbox": mapboxTiles,
+  	"Satellite": googleSat,
+  	"Terrain": googleTerrain
 };
 
 const overlays = {};
 
-// on ajoute le controleur en haut a droite pour changer de couche à notre guise
+// Ajout du controleur en haut a droite pour changer de couche
 L.control.layers(baseMaps, overlays).addTo(myMap);
 
-// ajout de l'échelle à la carte
+// Ajout de l'échelle à la carte
 
 L.control.scale({
   position: 'bottomleft'
@@ -68,152 +74,186 @@ L.control.scale({
 
 
 ///Création de nos icon///
-// dans un premier temps on importe notre icone personnel
 
+/**
+ * Importation de l'icone personalisée
+ */
 var iconePerso = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/ssuter6/Geovis2/main/figs/icone_rouge.png',
-  iconSize: [28, 41]
+  	iconUrl: 'https://raw.githubusercontent.com/ssuter6/Geovis2/main/figs/icone_rouge.png',
+  	iconSize: [28, 41]
 });
 
 // ajout des markers: 
   // dans un premier temps on créer une fonction qui permet nous permettra de voir quel est le nom de chaque spots lorsque l'on clique dessus
   // on ajoute ensuite nos marker en utilisant ceux qu'on a créer nous-même)
 function onEachFeature(feature, layer) {
-  if (feature.properties) {
-      layer.bindPopup("<h1>" + feature.properties.Nom + "</h1>" +
-       "<h4>" + feature.properties.Desc +"</h4>"+ "<h2>Type de voies: </h2><h4>" +
+  	if (feature.properties) {
+      	layer.bindPopup("<h1>" + feature.properties.Nom + "</h1>" +
+       	"<h4>" + feature.properties.Desc +"</h4>"+ "<h2>Type de voies: </h2><h4>" +
         feature.properties.Type_voies + "</h4>" +'>Press for more INFO </button>');
-      }
-  }
+    }
+}
+
 var lieux_grimpe = new L.geoJson(spots, {
-  onEachFeature: onEachFeature,
-  pointToLayer: function(feature,latlng){
-    return L.marker(latlng,{icon: iconePerso});
-  }
+  	onEachFeature: onEachFeature,
+  	pointToLayer: function(feature,latlng){
+    	return L.marker(latlng,{icon: iconePerso});
+  	}
 }).addTo(myMap);
 
+//////////////////////////////////////////////////
+/////////// OPEN TRIP PLANNER ////////////////////
+/////////////////////////////////////////////////
+
+// Ajout du controleur de notre position actuelle
 lc = L.control.locate().addTo(myMap);
 
+// Définition des liezux de départ et d'arrivée pour notre GPS
+// les 'from/to Name' mettront à jour les noms que l'utilisateurs voit (from et to)
 var fromSelectedPos = 'fromName';
+var toSelectedName ='toName';
+
+// les 'from/to Point' mettront à jour les coordonnées pour que OTP puisse calculer l'itinéraire
+// ces coordonnées sont invisibles pour l'utilisateur
 var fromCurrentPos = 'fromPoint'
 var toSelectedMarker = 'toPoint';
-var toSelectedName ='toName';
+
 var popup = L.popup();
 
-// on va chercher notre localisation actuelle pour calculer l'itinéraire depuis notre position
+/**
+ * Calcul de notre localisation actuelle pour calculer l'itinéraire depuis notre position
+ */
 myMap.on('locationfound', function (evt) {
 
-  //on va chercher lat et long de notre position actuelle
-  var currentPos = evt.latlng;
+  	// lat / long de la position actuelle de l'utilisateur
+  	var currentPos = evt.latlng;
 
-  // on reformate le résulat et on le lie à notre boite de dialogue
-  // correspond au input invisible
-  $('#'+fromCurrentPos).val(currentPos.lat + ',' + currentPos.lng);
+  	// On reformate le résulat et on le lie à notre boite de dialogue
+  	// correspond au input invisible
+  	$('#'+fromCurrentPos).val(currentPos.lat + ',' + currentPos.lng);
 
-  fromCurrentPos == 'fromPoint';
+  	fromCurrentPos == 'fromPoint';
 
-  // correspond à l'input visible
-  $('#'+fromSelectedPos).val('Current position');
-  fromSelectedPos == 'fromName'
-  // notre position va être recaluclé toutes les 5 secondes
+  	// Correspond à l'input visible
+  	$('#'+fromSelectedPos).val('Current position');
+  	fromSelectedPos == 'fromName'
 });
 
 
 var origin = null
-// définition du starting point si on veut autre chose que notre position actuelle
+
+/**
+ * Définition du starting point si autre que position actuelle
+ */
 function choseStartPoint (e) {
-  var currentPos = e.latlng;
-  if (origin != null){
-    myMap.removeLayer(origin);
-  }
-  // ajout un marqueur pour indiquer la position qu'on a choisi
-  origin = L.circleMarker(
-    [currentPos.lat, currentPos.lng],
-    {
-      color: '#FFFFFF',
-      fillOpacity: 1,
-      fillColor: '#ff2828'
-    }
-  ).addTo(myMap);
-  $('#'+fromCurrentPos).val(currentPos.lat + ',' + currentPos.lng);
-  fromCurrentPos == 'fromPoint';
+  	var currentPos = e.latlng;
+  	if (origin != null){
+    	myMap.removeLayer(origin);
+  	}
 
-  $('#'+fromSelectedPos).val('Selected position');
-  fromSelectedPos == 'fromName'
+  // On désactive locate control pour qu'il update pas si on choisit selectedPos
+  // sinon il remplace la position sélectionnée par notre pos actuelle
 
-  origin.bindPopup("Selected start point");
+  	lc._deactivate()
+
+  // Ajout un marqueur pour indiquer la position qu'on a choisi
+  	origin = L.circleMarker(
+    	[currentPos.lat, currentPos.lng],
+    	{
+      		color: '#FFFFFF',
+      		fillOpacity: 1,
+      		fillColor: '#ff2828'
+    	}
+  	).addTo(myMap);
+
+  	$('#'+fromCurrentPos).val(currentPos.lat + ',' + currentPos.lng);
+  	fromCurrentPos == 'fromPoint';
+
+  	$('#'+fromSelectedPos).val('Selected position');
+  	fromSelectedPos == 'fromName'
+
+  	origin.bindPopup("Selected start point");
 }
 
-// on va chercher notre lieu de destination
+/**
+ * Lieu de destination (uniquement marqueur sélectionnable)
+ */
 lieux_grimpe.on('click', function(e){
 
-  // on va chercher les coordonnées lat long du marqueur sur lequel on a cliqué
-  var pt = e.latlng;
-  //on va chercher le nom du marqueur sur lequel on a cliqué
-  var content = e.layer.feature.properties.Nom;
-  // puis on ajoute les coordonnées lat long du marqueur dans l'input hidden pour calculer l'itinéraire
-  $('#'+toSelectedMarker).val(pt.lat + ',' + pt.lng);
+  	// Coordonnées lat long du marqueur sur lequel on a cliqué
+  	var pt = e.latlng;
+  	//Nom du marqueur sur lequel on a cliqué (lieu de grimpe)
+  	var content = e.layer.feature.properties.Nom;
+  	// Ajout des coordonnées lat long du marqueur dans l'input hidden pour calculer l'itinéraire
+  	$('#'+toSelectedMarker).val(pt.lat + ',' + pt.lng);
   
-  toSelectedMarker == 'toPoint';
+  	toSelectedMarker == 'toPoint';
 
-  // on change le text de l'input pour qu'il corresponde au site sur lequel on a cliqué
-  $('#'+toSelectedName).val(content);
-  toSelectedName == 'toName';
+  	// Changement du texte de l'input pour qu'il corresponde au site sur lequel on a cliqué
+  	$('#'+toSelectedName).val(content);
+  	toSelectedName == 'toName';
 });
 
+/**
+ * Calcule de la route (starting point to ending point)
+ * et récupération des paramètres voulus pour OTP (makeRoutingQuery)
+ */
 function calculateRoute(){
-  // on prend le point de départ
-  var fromPoint = $('#fromPoint').val();
-  // et le point d'arrivée
-  var toPoint = $('#toPoint').val();
+  	// Point de départ
+  	var fromPoint = $('#fromPoint').val();
+  	// Point d'arrivée
+  	var toPoint = $('#toPoint').val();
 
-  // on va chercher le mode de transport choisi par l'utilisateur
-  // dans le dropdown menu
-  var chosenMode = $('#mode-select').val();
+  	// Mode de transport choisi par l'utilisateur
+  	var chosenMode = $('#mode-select').val();
 
-   // on fait le Query de notre route 
-   makeRoutingQuery({
-    fromPlace: fromPoint,
-    toPlace: toPoint,
-    time: '13:45',
-    date: '2021-10-20',
-    mode: chosenMode
-  });
+   	// Query de notre route
+   	makeRoutingQuery({
+    	fromPlace: fromPoint,
+    	toPlace: toPoint,
+    	time: '13:45',
+    	date: '2021-10-20',
+    	mode: chosenMode
+  	});
 }
 
-// s'il est possible de faire la route, on la dessine avec la fonction drawRoute
+/**
+ * Indication du chemin pour OTP
+ */
 function makeRoutingQuery(data){
-  $.ajax({
-    url: 'http://localhost:8080/otp/routers/ch/plan',
-    type: 'GET',
-    dataType: 'json',
-    data: data,
-    success: drawRoute,
-    error: calculateRouteError,
-    beforeSend: setHeader
-  });
+  	$.ajax({
+    	url: 'http://localhost:8080/otp/routers/ch/plan',
+    	type: 'GET',
+    	dataType: 'json',
+    	data: data,
+    	success: drawRoute,
+    	error: calculateRouteError,
+    	beforeSend: setHeader
+  	});
 }
 
 function setHeader(xhr){
-  xhr.setRequestHeader('Accept', 'application/json');
+  	xhr.setRequestHeader('Accept', 'application/json');
 }
 
-/////////////////////////////////
-///  DESSINER ITIN /////////////
-//////////////////////////////
+/////////////////////////////////////////////
+//////////  DESSINER ITINERAIRE /////////////
+/////////////////////////////////////////////
 
-// on crée une variable qui contiendra l'ensemble de nos polylines
+/**
+ * Variable contenant l'ensemble de nos polylines (groupées dans une couche)
+ */
 var polylineGroup = L.layerGroup().addTo(myMap);
 
 function drawRoute(data){
-  if (data.error){
-    alert(data.error.msg);
-    return;
-  }
+  	if (data.error){
+    	alert(data.error.msg);
+    	return;
+  	}
 
-  // on enlève tout d'abord notre polylinegroup pour que les itinéraires
-  // se redessine à chaque fois qu'on les recalcule
-  polylineGroup.clearLayers();
+  	// on enlève notre polylinegroup pour que les itinéraires
+  	// se redessine à chaque fois qu'on les recalcule
+  	polylineGroup.clearLayers();
   
     // problème avec OTP 2 qui renvoie comme premier itinéraire pour TRANSIT,WALK
     // uniquement le chemin à pied
@@ -231,103 +271,110 @@ function drawRoute(data){
     console.log('data', data)
     console.log('itin', itin)
     for (var i=0; i < itin.legs.length; i++){
-    // on va chercher chaque point géométrique que otp nous propose
-      var leg = itin.legs[i];
+    	// on va chercher chaque point géométrique que OTP nous propose
+      	var leg = itin.legs[i];
 
-      // on va chercher le mode de transport pour chaque leg (pour icone surement)
-      var leg_mode = leg.mode
-      console.log(leg_mode)
-      var steps = leg.steps;
+      	// on va chercher le mode de transport pour chaque leg (pour icone surement)
+      	var leg_mode = leg.mode
+      	console.log(leg_mode)
+      	var steps = leg.steps;
         for (var f = 0; f < steps.length; f++){
-          var step = steps[f];
-          //console.log(step)
+          	var step = steps[f];
+          	//console.log(step)
         }
-        // on le convertir en geojson
+        // on c$onvertit chaque point en geojson
         var geomLeg = polyline.toGeoJSON(leg.legGeometry.points);
-        // on stylise notre polyline geojson
-        // il faut grouper les layers ensemble
-        // ici on a notre couche qui rassemble l'ensemble de nos polyline
-      geojsonLayer = L.geoJSON(geomLeg, {
-        style: {
-            //border: 10,
-            "color": setColor(leg_mode),
-            "opacity": 0.9,
-            "weight": 8,
-            "dashArray": setDash(leg_mode)
-        }
+
+        // Style de notre polyline
+      	geojsonLayer = L.geoJSON(geomLeg, {
+        	style: {
+            	//border: 10,
+            	"color": setColor(leg_mode),
+            	"opacity": 0.9,
+            	"weight": 8,
+            	"dashArray": setDash(leg_mode)
+        	}
         // on ajoute chaque polyline à notre layergroup
-      }).addTo(polylineGroup);
-      //console.log(geojsonLayer)
+      	}).addTo(polylineGroup);
+      	//console.log(geojsonLayer)
     }
-  //}
-  // on va chercher la durée en minutes de notre trajet
-  // var polylineGroup = L.layerGroup().addLayer(geojsonLayer);
-  // console.log('group', polylineGroup)
-  var duree = itin.duration/60
-  // on va chercher la distance en km de notre trajet
-  var dist = itin.walkDistance/1000
-  // on va chercher les étapes de notre trajet
-  console.log(duree)
-  console.log(dist)
+
+  	// Durée en minutes du trajet
+  	var duree = itin.duration/60
+  	// Distance en km du trajet
+  	var dist = itin.walkDistance/1000
+  	console.log(duree)
+  	console.log(dist)
 }
 
-// fonction pour choisir le deuxième itin si TRANSIT,WALK est choisi
-// et premier itin pour tous les autres modes
+/**
+ * Fonction switch pour TRANSIT,WALk et tous les autres modes
+ * @param {*} mode Mode de transport choisi par l'utilisateur
+ * @returns deuxième itin pour TRANSIT,WALK, premier itin pour tous les autres
+ */
 function choseItin(mode) {
-  switch (mode) {
+  	switch (mode) {
 
-    case 'TRANSIT,WALK':
-      return 1;
+    	case 'TRANSIT,WALK':
+      		return 1;
 
-    default:
-      return 0;
-  }
+    	default:
+      		return 0;
+  	}
 }
 
-// défini la couleur de la polyline selon le mode transport
+/**
+ * Définition de la couleur des polylines selon le mode de transport
+ * @param {*} mode Mode de transport du leg
+ * @returns Couleur selon le mode de transport
+ */
 function setColor(mode) {
-  switch (mode) {
+  	switch (mode) {
 
-    case 'CAR':
-      return '#3f9cff';
+    	case 'CAR':
+      		return '#3f9cff';
     
-    case 'BICYCLE':
-      return '#73ff3f';
+    	case 'BICYCLE':
+      		return '#73ff3f';
     
-    case 'WALK':
-      return '#3f9cff';
+    	case 'WALK':
+      		return '#3f9cff';
     
-    case 'RAIL':
-      return ' #ff3f3f';
+    	case 'RAIL':
+      		return ' #ff3f3f';
 
-    case 'BUS':
-      return '#ffb33f';
+    	case 'BUS':
+      		return '#ffb33f';
 
-    case 'GONDOLA':
-      return '#82ff3f';
+    	case 'GONDOLA':
+      		return '#82ff3f';
 
-    case 'SUBWAY':
-      return '#ecff08';
+    	case 'SUBWAY':
+      		return '#ecff08';
 
-    default:
-      return 'white';
+    	default:
+      		return 'white';
 
-  }
+  	}	
 }
 
-// si mode WALK est choisi, met l'itin en traitillé
+/**
+ * Traitillé pour mode de transport = WALK
+ * @param {*} mode Mode de transport choisi
+ * @returns Traitillé si mode = WALK, null si autre moyen de transport
+ */
 function setDash(mode) {
-  switch(mode) {
+  	switch(mode) {
 
-    case 'WALK':
-      return '1, 15';
+    	case 'WALK':
+      		return '1, 15';
 
-    default:
-      return 'null';
-  }
+    	default:
+      		return 'null';
+  	}
 }
 
 function calculateRouteError(error){
-  alert('Error during route calculation.');
-  console.log('Routing error', error);
+  	alert('Error during route calculation.');
+  	console.log('Routing error', error);
 }
